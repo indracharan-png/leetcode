8e638aaf6b531ef90862e6283f06a68bfd2b69c2
Indra Charan <indracharan000@gmail.com>
2026-09-15 20:44:55 -0700

    Solved leetcode_105, was bit tricky. Refer to the code comments for explanation.

---
+++ trees/leetcode_105.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: list[int], inorder: list[int]) -> TreeNode | None:
+        index_of = {}
+        # Set up the indices map for the preorder array
+        n = len(inorder)
+        for i in range(n):
+            index_of[inorder[i]] = i
+
+        def makeTree(preorder, pre_left, pre_right, inorder, in_left, in_right) -> Optional[TreeNode]:
+            # Base case
+            if in_left > in_right:
+                return None
+            
+            # The element at the start of preorder sub-array will always be the root element (preorder:- root:left:right)
+            new_node = TreeNode(preorder[pre_left])
+            in_order_index = index_of[new_node.val] # Found out its index in inorder array using dict
+            # Now all the elements residing left of index of root will be in left sub-tree (inorder:- left:root:right)
+            left_len = in_order_index - in_left
+            # Same as above all the elements residing right of index of root will be in right sub-tree (inorder:- left:root:right)
+            right_len = in_right - in_order_index
+
+            # Update the next left and right recursive function calls accordingly
+            new_node.left = makeTree(preorder, pre_left + 1, pre_left + left_len, inorder, in_left, in_order_index - 1)
+            new_node.right = makeTree(preorder, pre_left + left_len + 1, pre_right, inorder, in_order_index + 1, in_right)
+
+            return new_node
+
+        return makeTree(preorder, 0, n - 1, inorder, 0, n - 1)
+
+          
