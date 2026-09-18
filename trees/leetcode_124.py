988ee380b3dfa2d3a9fac89bec3a2da0e11b918a
Indra Charan <indracharan000@gmail.com>
2026-09-17 21:16:06 -0700

    Solved leetcode_124, post order traversal is the idea but theres a small trick in updating the output val

---
+++ trees/leetcode_124.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxPathSum(self, root: TreeNode | None) -> int:
+
+        output = float('-inf')
+
+        def postOrder(root: Optional[TreeNode]) -> int:
+
+            nonlocal output 
+
+            # Base case 
+            if root is None:
+                return 0
+            
+            # Compute the max path sum from left subtree
+            left_val = postOrder(root.left)
+            # Compute the max path sum from right subtree
+            right_val = postOrder(root.right)
+
+            # Consider whichever is the greatest b/w left and right subtree max path sum values
+            max_val = max(left_val, right_val, 0) # Check if max_val is decreasing the actual root value or not
+
+            
+            max_val = max(max_val, 0)
+
+            output = max(output, root.val + max(left_val, 0) + max(right_val, 0)) # Update the output
+
+            return root.val + max_val
+
+
+        postOrder(root)
+        return int(output)
+  
+
+
+        
